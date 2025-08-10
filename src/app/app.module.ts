6eeccd874b7526fc1a4443a0cce5d0fba308136a
RaulRagel <raul.rs3006@gmail.com>
2025-08-11 00:12:24 +0200

    dartboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DartBoardComponent } from './components/dartboard/dartboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DartBoardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/dartboard/dartboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dartboard',
+  templateUrl: './dartboard.component.html',
+  styleUrls: ['./dartboard.component.css'],
+})
+export class DartBoardComponent implements OnInit {
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
