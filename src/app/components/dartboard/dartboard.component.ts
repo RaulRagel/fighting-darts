f639b4413939396e2a2a398ca61f2812adc479a7
RaulRagel <raul.rs3006@gmail.com>
2025-08-11 10:51:29 +0200

    agregados lanzamientos

--- src/app/components/dartboard/dartboard.component.ts
+++ src/app/components/dartboard/dartboard.component.ts
@@ -7,15 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DartBoardComponent implements OnInit {
 
-  // throws: Array[] = 
+  throws: String[] = [];
+  dartboardSections!: NodeList;
 
   constructor() {
     
   }
 
   ngOnInit(): void {
-    
+
+    this.dartboardSections = document.querySelectorAll('#dartboard #areas g path, #dartboard #areas g circle');
+    console.log('init boardSections', this.dartboardSections);
+
+    this.dartboardSections.forEach(p =>
+      p.addEventListener('click', (event) => this.boardClickHandler(event)) // evitamos perder el contexto
+    );
   }
+
+  boardClickHandler(event: any) {
+    this.addDart(event.target.getAttribute('id'));
+  }
+
+  addDart(id: string) {
+    this.throws.push(id);
+    console.log('added dart', this.throws);
+  }
+
+  // getDartValueFromID(id: string) {
+  //   if (!id) return 0;
+    
+  //   if (id == 'Bull') return 50;
+  //   if (id == 'Outer') return 25;
+    
+  //   let mod = 0;
+  //   switch(id[0]) {
+  //       case 's': mod = 1; break;
+  //       case 'd': mod = 2; break;
+  //       case 't': mod = 3; break;
+  //       default: mod = 1;
+  //   }
+    
+  //   return mod * parseInt(id.slice(1));
+  // }
+    
 }
 
 
